6eb986a9be45876d82fc8cf4b094b50ff7ac0055
A B O U D <aboudkalaf10@gmail.com>
2024-12-27 15:16:09 +0300

    implicit animations ended and refactor the folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:anmations/animations/animated_aligment_animation_page.dart';
-import 'package:anmations/animations/animated_container_animation_page.dart';
-import 'package:anmations/animations/animated_cross_fade_animation_page.dart';
-import 'package:anmations/animations/animated_list_animation_page.dart';
-import 'package:anmations/animations/animated_opacity_animation_page.dart';
-import 'package:anmations/animations/animated_pading_animation_page.dart';
-import 'package:anmations/animations/animated_physical_model_animation_page.dart';
-import 'package:anmations/animations/animated_positioned_animation_page.dart';
-import 'package:anmations/animations/animated_positioned_directional_animation_page.dart';
-import 'package:anmations/animations/animated_switcher_animation_page.dart';
-import 'package:anmations/animations/animated_text_style_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_aligment_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_container_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_cross_fade_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_list_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_opacity_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_pading_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_physical_model_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_positioned_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_positioned_directional_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_switcher_animation_page.dart';
+import 'package:anmations/animations/implicit_animations/animated_text_style_animation_page.dart';
 import 'package:anmations/models/page.dart';
 import 'package:flutter/material.dart';
 
